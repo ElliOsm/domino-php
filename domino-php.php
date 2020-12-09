2c3d461ac6ea74854d6de0656e7a8b6fd24753a4
Elli Eirini Osmantzikidou <elli.osmantzikidou@gmail.com>
2020-12-09 15:29:23 +0200

    Add files via upload

--- domino-php.php
+++ domino-php.php
@@ -1,19 +1,38 @@
 <?php 
     testingGrounds();
-
     function testingGrounds(){
         $state = dominoState([0,1]);
-
+        echo "hand b4 drawing a domino: ";
+        echo '<br />';
         printCurrentPlayerHand($state);
-
+        echo '<br />';
+        echo "hand after drawing a domino: ";
+        echo '<br />';
         $state = takeFromPile($state);
         printCurrentPlayerHand($state);
-
-
+        echo '<br />';
         $state = playDomino($state, 3);
-
+        echo '<br />';        
+        echo "board after playing a domino: ";
+        echo '<br />';
         printBoard($state);
-        //$state = playDomino($state, 5);
+        echo '<br />';
+        $state = playDomino($state, 2);
+        echo '<br />';        
+        echo "board after playing a second domino: ";
+        echo '<br />';
+        printBoard($state);
+        echo '<br />';
+        echo "hand after playing a domino: ";
+        echo '<br />';     
+        printCurrentPlayerHand($state);
+        echo '<br />'; 
+        $state = playDomino($state, 3);
+        echo '<br />';        
+        echo "board after playing a third domino: ";
+        echo '<br />';
+        printBoard($state);
+ 
     }
 
     //creating an assosiative array 
@@ -44,29 +63,45 @@
             $new[$key] = $deck[$key];
         }
         $deck = $new;
-
         return $deck;
     }
 
-    //state initialiser function 
+    //state initializer function 
     function dominoState($playerIds) {
         $deck = deck();
         $startingDeck = shuffleDeck($deck);
-
         $state = ["deck" => $startingDeck,
             "players" => 
             [   0 =>[ "id" => $playerIds[0],
-                        "hand" => take(7, $startingDeck)],
+                        "hand" => take(6, $startingDeck)],
                 1 =>[ "id" => $playerIds[1],
-                        "hand" => take(7, drop(7, $startingDeck))]
+                        "hand" => take(6, drop(6, $startingDeck))]
             ],
-            "board" => take(1, drop(14, $startingDeck)),
-            "pile" => drop(15, $startingDeck),
+            "board" => array(),
+            "pile" => drop(12, $startingDeck),
             "current-player" => 0 // only 0 or 1. i use this for also indexing the players
         ];
+        //$state = chooseWhoPlaysFirst($state);
         return $state;
     }
 
+    //call at the start of the game if we need a redraw
+    function redraw($state){
+        $deck = deck();
+        $startingDeck = shuffleDeck($deck);
+        $state["players"][0]["hand"] = take(6, $startingDeck);
+        $state["players"][1]["hand"] = take(6, drop(6, $startingDeck));
+        $state["pile"] = drop(12, $startingDeck);
+        $state = chooseWhoPlaysFirst($state);
+        return $state;
+    }
+
+    function chooseWhoPlaysFirst($state){
+        $randIndex = rand(0,1);
+        $state["current-player"] = $randIndex;
+        return $state;
+
+    }
     function getCurrentPlayerHand($state) {
         $playerIndex = $state["current-player"];
         return $state["players"][$playerIndex]["hand"];
@@ -80,28 +115,28 @@
 
     function addDominoToPlayer($state, $domino) {
         $playerIndex = $state["current-player"];
-        array_push($state["players"][$playerIndex]["hand"] , $domino);
+        array_push($state["players"][$playerIndex]["hand"] , ...$domino);
         return $state;
     }
 
-    // TODO: FIX THIS 
+
     function removeDominoFromPlayer($state, $domino) {
         $oldHand = getCurrentPlayerHand($state);
-        $newHand = array_filter($oldHand, isDominoThere($oldHand, $domino));
+        $newHand = array_filter($oldHand,  function($oldHand) use($domino){
+            $tempFront = $oldHand["front"];
+            $tempBack = $oldHand["back"];
+            return $oldHand["front"] != $domino["front"] || $oldHand["back"] != $domino["back"];
+        });
+        return setCurrentPlayerHand($state,$newHand); 
+    }
         
-        return setCurrentPlayerHand($state,$newHand); //keep all domino that are not the same as the given domino
-    }
-
-    function isDominoThere($arr, $domino){
-        return $arr["front"] === $domino["front"] && $arr["back"] === $domino["back"]; 
-    }
 
     //remove the top domino from the pile, update the pile and return it via state
     function takeFromPile($state) {
         $oldPile = $state["pile"];
         $state["pile"] = drop(1, $oldPile);
-        
         $state = addDominoToPlayer($state, take(1, $oldPile));
+        isItOver($state);
         return $state;
     }   
     
@@ -122,16 +157,22 @@
     
     function addDominoToBoard($state, $domino) {
         $board = $state["board"];
-        $lastElement =  array_pop($board);
-        $firstElemet = array_shift($board);
-        if($domino["front"] == $lastElement["back"]){
+        if (empty($board)){
+            $state = removeDominoFromPlayer($state, $domino);
             array_push($state["board"],$domino);
-            $state = removeDominoFromPlayer($state, $domino);
-        }elseif($domino["back"] == $lastElement["front"]){
-            array_unshift($state["board"],$domino);
-            $state = removeDominoFromPlayer($state, $domino);
         }else{
-            echo ("invalid play");
+            $lastElement =  array_pop($board);
+            $firstElemet = array_shift($board);
+            if($domino["front"] == $lastElement["back"]){
+                $state = removeDominoFromPlayer($state, $domino);
+                array_push($state["board"],$domino);
+            }elseif($domino["back"] == $lastElement["front"]){
+                $state = removeDominoFromPlayer($state, $domino);
+                array_unshift($state["board"],$domino);
+            }else{
+                //add maybe a pop up window for illegal move
+                echo ("invalid play");
+            }
         }
         return $state;
     }
@@ -139,19 +180,28 @@
     function playDomino($state, $pos){
         $playerIndex = $state["current-player"];
         $domino = $state["players"][$playerIndex]["hand"][$pos];
-        
         $state  = addDominoToBoard($state,$domino);
         isItOver($state);
-        return nextTurn($state);
+        return nextTurn($state); //to change player turns
     }
 
     function isItOver($state){
         $hand = getCurrentPlayerHand($state);
-        if($hand == null){
+        $pile = $state["pile"];
+        if(empty($hand)){
             echo "The Game is over! ". $state["current-player"] . " won!";
             //maybe freeze all html elements so he cant make any more moves?
+        }elseif(empty($pile)){
+            //countPoints($state);
         }
     }
+    /** 
+    function countPoints($state){
+        $player1Hand =  $state["players"][0]["hand"];
+        $player2Hand =  $state["players"][1["hand"];
+        foreach()
 
+    }
+*/
 
 ?>
