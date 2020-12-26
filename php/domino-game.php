ee35e2e0cc577b9cd5580bbbdf0fed87ee1847e2
Elli Eirini Osmantzikidou <elli.osmantzikidou@gmail.com>
2020-12-26 17:46:11 +0200

    Updated functions

--- php/domino-game.php
+++ php/domino-game.php
@@ -1,11 +1,29 @@
 <?php
 include 'domino-function-library.php';
+include 'state-sql.php';
 
+//connect to sql
+if(!isset($connected)||$connected == false){
+    require "dbconnect.php";
+}
+
+//create session
 if (session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
-$state = dominoState(["Jack","Jill"]);
+//get session user 1 and 2
+$_SESSION['id'] = uniqid(); //generates a unique id
+$player1 = $_SESSION['user'];
+$player2 = $_SESSION['user2'];
+$_SESSION['id'];
+
+
+
+$state = dominoState($player1,$player2);
+createTableForState($state);
+
+//preparation
 echo '<br />';
 while($state["end"] == FALSE){
     echo 'The player who plays first is ' . getCurrentPlayerId($state); " .";
@@ -18,8 +36,13 @@ while($state["end"] == FALSE){
     echo '<br />';
     printBoard($state);
     echo '<br />';
-    //input
-    
+    //USER INPUT
+    $hand = getCurrentPlayerHand($state);
+    $domino = array_pop($hand);
+    $front = $domino["front"];
+    $back = $domino["back"];
+    $state = playDomino($state,$front,$back);
+    updateTableFromState($state);
 
 }
 
